ec53674021f9aae5b197e12aa6434ebf67c89fd6
Yusuke Wada <yusukebe@cpan.org>
2010-03-21 23:46:04 +0900

    mv.

--- eg/app.pl
+++ eg/app.pl
@@ -3,6 +3,6 @@ use OpenSocialX::Gadgets;
 
 my $gadget = OpenSocialX::Gadgets->new( title => 'Hello World', description => 'just hello world' );
 $gadget->add_pref( 'Require', { feature => ['opensocial-0.8'] } );
-$gadget->add_content( { type => 'html', view => 'canvas', include => [qw!./eg/hello.html ./eg/hello.js!] } );
+$gadget->add_content( { type => 'html', view => 'canvas', include => [qw!./eg/app.html ./eg/app.js!] } );
 my $xml = $gadget->render;
 print $xml;
